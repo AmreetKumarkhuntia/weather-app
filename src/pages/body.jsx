1822490c3dd87a63665ed8d6296f3af2461755fd
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2022-01-12 06:28:01 +0530

    geo tracker update and color update

--- src/pages/body.jsx
+++ src/pages/body.jsx
@@ -4,12 +4,12 @@ import { useState } from "react";
 
 
 const Body = () => {
-    const apikey=process.env.REACT_APP_API_KEY;
-    let lat = "65";
-    let lon = "65"
+    const apikey = process.env.REACT_APP_API_KEY;
+    let lat = "28.6139";
+    let lon = "77.2090"
     let lang = "en";
 
-    let [unit,setUnit]=useState('imperial');
+    let [unit, setUnit] = useState('imperial');
     const units = ["imperial", "metric"];
     const langs = ["en", "ar", "az", "be"];
 
@@ -29,6 +29,18 @@ const Body = () => {
                 {langs}
             </option>
         )
+    }
+    function fetchlocation(){
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                lat=position.coords.latitude;
+                lon=position.coords.longitude;
+                alert("Current latitude set to "+lat+" and longitude to "+ lon);
+              });
+          } else {
+            alert("Geo Tracker is not Available");
+          }
+
     }
     function fetchweather() {
         var url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=";
@@ -51,39 +63,57 @@ const Body = () => {
     return (
         <div className="weather">
             <h1 className="parameter_title">ENTER PARAMETERS</h1>
-            <div className="get-weatherforecast">
-                <div className="titles">
-                    Enter Latitude:<br />
-                    Enter Longitude:<br />
-                    Select units:<br />
-                    Select Language:<br />
-                </div>
-                <div className="parameters">
-                    <input type="text" className="input" placeholder="65" onChange={(e) => {
-                        lat = e.target.value;
-                    }}></input><br />
-                    <input type="text" className="input" placeholder="65" onChange={(e) => {
-                        lon = e.target.value;
-                    }}></input><br />
-                    <select className="input" onClick={(e) => {
-                        setUnit(e.target.value);
-                    }}>
-                        {units.map(unitmapping)}
-                    </select><br />
-                    <select className="input" onClick={(e) => {
-                        lang = e.target.value;
-                    }}>
-                        {langs.map(langmapping)}
-                    </select><br />
-                </div>
+            <table className="get-weatherforecast">
+                <tr>
+                    <td className="titles">Enter Latitude:</td>
+                    <td className="parameters">
+                        <input type="text" className="input" placeholder={lat} onChange={(e) => {
+                            lat = e.target.value;
+                        }} />
+                    </td>
+                </tr>
+                <tr>
+                    <td className="titles">Enter Longitude:</td>
+                    <td className="parameters" >
+                        <input type="text" className="input" placeholder={lon} onChange={(e) => {
+                            lon = e.target.value;
+                        }} />
+                    </td>
+                </tr>
+                <tr>
+                    <td className="titles">Select units:</td>
+                    <td className="parameters">
+                        <select className="input" onClick={(e) => {
+                            setUnit(e.target.value);
+                        }}>
+                            {units.map(unitmapping)}
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="titles">Select Language:</td>
+                    <td className="parameters">
+                        <select className="input" onClick={(e) => {
+                            lang = e.target.value;
+                        }}>
+                            {langs.map(langmapping)}
+                        </select>
+                    </td>
+                </tr>
+
+            </table>
+            <div className="div-button">
+                <button onClick={()=>{
+                    fetchlocation();
+                }} >
+                    Get Location
+                </button>
+                <button onClick={() => {
+                    fetchweather();
+                }}>Get Weather</button>
             </div>
-            <button onClick={() => {
-                fetchweather();
-            }}>Get Weather</button>
-            <div className="separator" />
-            <div>
-                {dataisloaded&&<Showweather unit={unit} Dataisloaded={dataisloaded} items={items} />}
-            </div>
+            {dataisloaded && <div className="separator" />}
+            {dataisloaded && <Showweather unit={unit} Dataisloaded={dataisloaded} items={items} />}
         </div>
     );
 }
